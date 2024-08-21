c3798339d1cad1f7ee80ae8f6b22189ab5f2602a
Parshwa <parshwashah282011@gmail.com>
2024-08-20 23:01:31 -0700

    first version to show

--- item-brand/data/index.js
+++ item-brand/data/index.js
@@ -1,17 +1,42 @@
 const url="http://73.97.237.79:8000";
-function getData(){
+const url2="http://192.168.43.35:8000";
+function getData1(){
     var request = new XMLHttpRequest()
+    request.onerror = (error) => {
+        alert("there was errror")
+    }
     request.responseText = "text/plain"
     request.onload = (res) => {
-        console.log(JSON.parse(request.responseText))
         loadpage(JSON.parse(request.responseText))
     }
-    request.open("GET",url+"/?query="+document.getElementById("q").innerHTML)
+    request.open("GET",url2+"/?query="+document.getElementById("q").innerHTML)
+}
+function getData(){
+    var request = new XMLHttpRequest()
+    request.onerror = (error) => {
+        getData1()
+    }
+    request.ontimeout = ()=>{
+        getData1()
+    }
+    request.onabort = ()=>{
+        getData1()
+    }
+    request.responseText = "text/plain"
+    request.onload = (res) => {
+        loadpage(JSON.parse(request.responseText))
+    }
+    if(window.location.href.startsWith("http://192.168.43.35")){
+        request.open("GET",url2+"/?query="+document.getElementById("q").innerHTML)
+    }else{
+        request.open("GET",url+"/?query="+document.getElementById("q").innerHTML)
+    }
     request.send()
-
 }
 function GETData(){
-    window.location.replace(window.location.href+"?query="+document.getElementById("query").value)
+    if(window.location.search != undefined){
+        window.location.replace(window.location.href.split("?")[0]+"?query="+document.getElementById("query").value)
+    }
 }
 function bodyonload(){
     var weburl = new URL(window.location.href)
@@ -23,7 +48,6 @@ function bodyonload(){
     }else{
         outputarea.style.visibility = "hidden";
     }
-    console.log("loaded")
 }
 function tobarcode(){
     window.location.replace("http://"+window.location.host+"/barcode")
@@ -34,14 +58,26 @@ function toitembrand(){
 function loadpage(data){
     startrow = "<tr id=\"datarow\"></tr>"
     rows = document.getElementById("toprow").outerHTML
+    rows2 = document.getElementById("toprow2").outerHTML
+    console.log(rows)
+    console.log(rows2)
     for(i in data){
         parts = ""
+        parts2 = ""
+        var a = 0
         for(k in data[i]){
-            parts += "<td>"+data[i][k]+"</td>"
+            a += 1;
+            if(a<4){
+                parts += "<td>"+data[i][k]+"</td>";
+            } else {
+                parts2 += "<td>"+data[i][k]+"</td>"
+            }
         }
         rows += "<tr id=\"datarow\">"+parts+"</tr>"
-        console.log(parts)
+        rows2 += "<tr id=\"datarow\">"+parts2+"</tr>"
     }
-    console.log(rows)
     document.getElementById("toprow").outerHTML = rows
+    document.getElementById("toprow2").outerHTML = rows2
+    console.log(rows)
+    console.log(rows2)
 }
