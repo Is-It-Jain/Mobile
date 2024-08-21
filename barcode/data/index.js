e3672f329404fe29278cf66bc3478d89a61a794c
Parshwa <parshwashah282011@gmail.com>
2024-08-21 14:43:02 -0700

    adding image test 1

--- barcode/data/index.js
+++ barcode/data/index.js
@@ -1,28 +1,6 @@
 const url="https://us-west-2.aws.data.mongodb-api.com/app/barcode-ofdsbkb/endpoint/api";
-const url2="http://192.168.43.35:8001";
-var responsedata;
-function getData1(){
-    var request = new XMLHttpRequest()
-    request.onerror = (error) => {
-        alert("there was errror")
-    }
-    request.responseText = "text/plain"
-    request.onload = (res) => {
-        loadpage(JSON.parse(request.responseText))
-    }
-    request.open("GET",url2+"/?q="+document.getElementById("q").innerHTML)
-}
 function getData(){
     var request = new XMLHttpRequest()
-    request.onerror = (error) => {
-        getData1()
-    }
-    request.ontimeout = ()=>{
-        getData1()
-    }
-    request.onabort = ()=>{
-        getData1()
-    }
     request.responseText = "text/plain"
     request.onload = (res) => {
         loadpage(JSON.parse(request.responseText))
@@ -66,8 +44,10 @@ function loadpage(data){
             a += 1;
             if(a<4){
                 parts += "<td>"+data[i][k]+"</td>";
-            } else {
+            } else if(a<6){
                 parts2 += "<td>"+data[i][k]+"</td>"
+            } else {
+                parts2 += "<td><img src="+data[i][k]+"></img></td>"
             }
         }
         rows += "<tr id=\"datarow\">"+parts+"</tr>"
--- item-brand/data/index.js
+++ item-brand/data/index.js
@@ -1,32 +1,11 @@
 const url="https://us-west-2.aws.data.mongodb-api.com/app/barcode-ofdsbkb/endpoint/api";
-const url2="http://192.168.43.35:8000";
-function getData1(){
-    var request = new XMLHttpRequest()
-    request.onerror = (error) => {
-        alert("there was errror")
-    }
-    request.responseText = "text/plain"
-    request.onload = (res) => {
-        loadpage(JSON.parse(request.responseText))
-    }
-    request.open("GET",url2+"/?query="+document.getElementById("q").innerHTML)
-}
 function getData(){
     var request = new XMLHttpRequest()
-    request.onerror = (error) => {
-        getData1()
-    }
-    request.ontimeout = ()=>{
-        getData1()
-    }
-    request.onabort = ()=>{
-        getData1()
-    }
     request.responseText = "text/plain"
     request.onload = (res) => {
         loadpage(JSON.parse(request.responseText))
     }
-    request.open("GET",url+"?query="+document.getElementById("q").innerHTML)
+    request.open("GET",url+"?q="+document.getElementById("q").innerHTML)
     request.send()
 }
 function GETData(){
